e3180a93fc5c0fde6e2fedac87bfae315af41170
Vivek Shukla <30733494+shukl500@users.noreply.github.com>
2018-07-16 22:50:58 +0200

    District Location API

---
+++ GetDistrict/GetDistrict/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetDistrict.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> districtName = new List<string>();
+            XmlDocument Data = new XmlDocument();
+            Data.Load("Districts.xml");
+            foreach( XmlNode singleNode in Data.DocumentElement.ChildNodes) 
+            {
+               districtName.Add(singleNode.InnerText);
+            }
+
+            //return new string[] { "value1", "value2" };
+            return districtName.AsEnumerable();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GetDistrict/GetDistrict/Districts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetDistrict
+{
+    public class Districts
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
